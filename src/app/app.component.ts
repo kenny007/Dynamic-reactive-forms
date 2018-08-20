fb18be1a2a60681840a70e504ad5c030bba9ba2d
Adebayo Kehinde <press.spark@gmail.com>
2018-08-21 01:24:04 +0200

    Improves dynamic form further

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template:`
+  <h1> Dynamic reactive form in angular </h1>
+  <app-dynamic-form [formDataObj]="person"></app-dynamic-form>
+  `
 })
 export class AppComponent {
   title = 'Angular';
+  person = {
+    firstname: {
+       label:'Firstname',
+       value:'Kehinde',
+       type: 'text'
+    },
+    age: {
+      label: 'Age',
+      value: 32,
+      type: 'number'
+    }
+  };
 }
--- src/app/reative-form/reative-form.component.ts
+++ src/app/reative-form/reative-form.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup,  FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamic-form',
   template: `
     <form [formGroup]="form">
     <div *ngFor="let prop of personProps">
-        <input type="text" [formControlName]="prop">
+      <label class="control-label"> {{prop.label}} </label>
+      <input [type]="prop.type" class="form-control" [formControlName]="prop.key">
     </div>
     </form>
     <pre>{{ form.value | json }}</pre>
@@ -15,19 +16,19 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class ReativeFormComponent implements OnInit {
   form: FormGroup;
-  person = {
-    firstname: 'Kehinde',
-    age: 32,
-    surname: 'Adebayo',
-    twitter:'@kennymore007'
-  };
+  @Input() formDataObj;
+
   personProps = [];
 
   ngOnInit() {
     const formDataObj = {};
-    for(const prop of Object.keys(this.person)) {
-      formDataObj[prop] = new FormControl(this.person[prop]);
-      //this.personProps.push(prop);
+    for(const prop of Object.keys(this.formDataObj)) {
+      formDataObj[prop] = new FormControl(this.formDataObj[prop].value);
+      this.personProps.push({
+        key: prop,
+        label: this.formDataObj[prop].label,
+        type: this.formDataObj[prop].type,
+      });
     }
 
     this.form = new FormGroup(formDataObj);
